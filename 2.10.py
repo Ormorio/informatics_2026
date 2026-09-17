76ce8cfe8a08b1bf96b34efe528271e07c111f6b
Ormorio <cherartem2008@gmail.com>
2026-09-17 19:25:21 +0300

    2.10 updated

--- 2.10.py
+++ 2.10.py
@@ -2,18 +2,17 @@ with open("input.txt", "r") as f:
     l = f.read()
 
 c = ""
-for i in range(len(l) - 1):
-    if l[i] in "уеёыаоэяию" and not(l[i + 1] in "уеёыаоэяию") and c != "":
-        c = l[i]
-        l = f"{l[:i]} 'с' {c} {l[i+1:]}"
-    elif l[i] in "уеёыаоэяию" and l[i - 1] in "йцкнгшщзхъфвпрлджчсмтьб" and c != "":
-        c = l[i]
-        l = f"{l[:i]} 'с' {c} {l[i+1:]}"
+k = ""
+for i in range(len(l)):
+        if l[i] in "уеёыаоэяиюУЕЁЫАОЭЯИЮ" and c == "":
+            c = l[i]
+            break
 
-if l[-1] in "уеёыаоэяию" and c != "":
-    c = l[-1]
-    l = f"{l[:-1]} 'с' {c}"
-
+for i in range(len(l)):
+    k += l[i]
+    if i  > 0 and l[i] in "уеёыаоэяиюУЕЁЫАОЭЯИЮ" and l[i - 1] in "йцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ" and c != "":
+        c = l[i]
+        k += "с" + c.lower()
 
 with open("input.txt", "w") as f:
-    f.write(l)
+    f.write(k)
